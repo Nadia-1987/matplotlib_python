0457ed8ccf954deddcfc238234c133cf0bad85ae
Nadia-1987 <63470922+Nadia-1987@users.noreply.github.com>
2020-08-04 00:05:32 -0300

    ejercicios clase graficos

--- ejercicios_clase.py
+++ ejercicios_clase.py
@@ -39,7 +39,15 @@ def ej1():
     # Graficar el "line plot" de "Y" en función de "X"
     # Colocar la leyenda y el label con el nombre de la función
     # Darle color a la línea a su elección
+    fig = plt.figure()     
+    ax = fig.add_subplot()  
 
+    ax.plot(x, y, color='r')           
+    ax.set_facecolor('whitesmoke')
+    ax.set_title("y en función x")
+    ax.set_ylabel("y")
+    ax.set_xlabel("x")
+    plt.show()
 
 def ej2():
     # Line Plot
@@ -64,6 +72,17 @@ def ej2():
 
     # Cada función dibujarla con un color distinto
     # a su elección
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    ax.plot(x, y1, color='b', label='y1 en funcion de x')
+    ax.plot(x, y2, color='r', label='y2 en funcion de x')
+    ax.set_facecolor('whitesmoke')
+    ax.set_title("y por cada valor de x")
+    ax.set_ylabel("Y")
+    ax.set_xlabel("X")
+    ax.legend()
+    plt.show()
 
 
 def ej3():
@@ -84,7 +103,17 @@ def ej3():
     # cada gráfico
 
     # Elegir un marker a elección
+    np.random.shuffle(x)
+    fig = plt.figure()
+    fig.suptitle('tangente hiperbólica', fontsize=10)
+    ax = fig.add_subplot()
 
+    ax.scatter(x, y, c='r', marker='+', label='funcion de tanh para los valores de x' )
+    ax.set_facecolor('whitesmoke')
+    ax.legend()
+    plt.show()
+
+   
 
 def ej4():
     # Figura con múltiples gráficos
@@ -122,12 +151,53 @@ def ej4():
     # a su elección
 
     # Colocar una grilla a elección
+    
+    # Dibujar 4 gráficos en una misma figura
+    fig = plt.figure()
+    # Ejemplo de uso --> ax = fig.add_subplot(nrows, ncols, index)
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax2 = fig.add_subplot(2, 2, 2)
+    ax3 = fig.add_subplot(2, 2, 3)
+    ax4 = fig.add_subplot(2, 2, 4)
 
+    ax1.plot(x, y1, color='b', marker='^', label="x al cuadrado")
+    ax1.set_facecolor('whitesmoke')
+    ax1.set_title("x al cuadrado")
+    ax1.set_ylabel("Y")
+    ax1.set_xlabel("X")
+    ax1.legend()
+    ax1.grid(ls='dashed')
+
+    ax2.plot(x, y2, color='c', marker='+', label='x al cubo')
+    ax2.set_facecolor('whitesmoke')
+    ax2.set_title("x a cubo")
+    ax2.set_ylabel("Y")
+    ax2.set_xlabel("X")
+    ax2.legend()
+    ax2.grid(ls='dashed')
+
+    ax3.plot(x, y3, color='g', marker='.', label='x a la cuarta')
+    ax3.set_facecolor('whitesmoke')
+    ax3.set_title("x a la cuarta")
+    ax3.set_ylabel("Y")
+    ax3.set_xlabel("X")
+    ax3.legend()
+    ax3.grid(ls='dashed')
+
+    ax4.plot(x, y4, color='k', label='raiz cuadrada de x')
+    ax4.set_facecolor('whitesmoke')
+    ax4.set_title("raiz cuadrada de x")  
+    ax4.set_ylabel("Y")
+    ax4.set_xlabel("X")
+    ax4.legend()
+    ax4.grid(ls='dashed')
+  
+    plt.show()
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    ej1()
-    # ej2()
+    # ej1()
+    #ej2()
     # ej2()
     # ej3()
-    # ej4()
+    ej4()
